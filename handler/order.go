92ba7a66a44bcbaccac5faa6e85f80e3e229eee8
Omkar-Kamat <mail4omkarkamat@gmail.com>
2026-05-14 20:32:43 +0530

    video 2

---
+++ handler/order.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Order struct{
+
+}
+
+func (o *Order) Create(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Create an order")
+}
+
+func (o *Order) List(w http.ResponseWriter, r *http.Request){
+	fmt.Println("List all order")
+}
+
+func (o *Order) GetByID(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Get order by ID")
+}
+
+func (o *Order) UpdateByID(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Update by ID")
+}
+
+func (o *Order) DeleteByID(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Delete by ID")
+}
--- main.go
+++ main.go
@@ -2,31 +2,16 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"context"
+	"github.com/Omkar-Kamat/go-pro/application"
 )
 
 func main(){
-	router := chi.NewRouter()
+	app := application.New()
 
-	router.Use(middleware.Logger)
-
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr: ":3000",
-		Handler: router,
-	}
-
-
-	err := server.ListenAndServe()
+	err := app.Start(context.TODO())
 
 	if err != nil{
-		fmt.Println("failed to listen to server", err)
+		fmt.Print("failed to start app:",err)
 	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Hello World"))
 }
